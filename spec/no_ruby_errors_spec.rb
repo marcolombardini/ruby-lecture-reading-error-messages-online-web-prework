5dc5f00f67809632fed2380dfe7c6295615e54f2
Marco Lombardini <marco.lombardini@gmail.com>
2019-02-22 01:56:06 +0000

    Automatically backed up by Learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,12 +1,11 @@
 describe "Not having any errors and being all green" do
 
   context 'NameError' do
-    it "raises a NameError when encountering undefined barewords" do
+    it 'raises a NameError when encountering undefined barewords' do
       expect{
         load './lib/a_name_error.rb'
       }.to_not raise_error
     end
-  end
 
   context 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code' do

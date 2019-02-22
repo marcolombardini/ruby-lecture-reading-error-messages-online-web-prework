a497fcda22e98948a4ce535e789b93d560e970aa
Marco Lombardini <marco.lombardini@gmail.com>
2019-02-22 02:03:07 +0000

    Done.

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -6,24 +6,21 @@ describe "Not having any errors and being all green" do
         load './lib/a_name_error.rb'
       }.to_not raise_error
     end
-  end
-
+end
   context 'SyntaxError' do
     it 'raises a SyntaxError for nonsensical code' do
       expect{
         load './lib/a_syntax_error.rb'
       }.to_not raise_error
     end
-  end
-
+end
   context 'TypeError' do
     it 'raises a TypeError for objects of the wrong type' do
       expect{
         load './lib/a_type_error.rb'
       }.to_not raise_error
     end
-  end
-
+end
   context 'ZeroDivisionError' do
     it 'raises a ZeroDivisionError for dividing by zero' do
       expect{
